e54c7c8df38b5e1f1a4e37eabb89d628e3e5fa71
jaso21 <jasosandoval78@gmail.com>
2024-02-14 18:35:32 -0500

    Creando nuestro proyecto

---
+++ app/config.php
@@ -0,0 +1,18 @@
+<?php
+    
+    define('SERVIDOR','localhost');
+    define('USUARIO','root');
+    define('PASSWORD','');
+    define('BD','tienda');
+
+    $servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+    try {
+        $pdo = new PDO($servidor,USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+        echo "La conexion a la bd fue exitosa";
+    } catch (PDOException $e) {
+        //throw $th;
+        //print_r($e);
+        echo "Error al conectar la base de datos";
+    }
+?>
